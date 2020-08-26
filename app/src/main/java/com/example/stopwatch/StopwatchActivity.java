aa2bf3d8d25aac2b0ee8ae96b46abfa73a97de6f
vsafontev <vladislavsafontev@gmail.com>
2020-08-26 08:42:13 +0300

    2 commit

--- app/src/main/java/com/example/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/example/stopwatch/StopwatchActivity.java
@@ -1,5 +1,6 @@
 package com.example.stopwatch;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,11 +13,17 @@ import java.util.Locale;
 public class StopwatchActivity extends AppCompatActivity {
     private int seconds = 0;
     private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
         runTimer();
     }
 
@@ -57,4 +64,43 @@ public class StopwatchActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    /*@Override
+     protected void onStop() {
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }*/
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    /*@Override
+    protected void onStart(){
+        super.onStart();
+        if (wasRunning) {
+            running = true;
+        }
+    }*/
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
 }
